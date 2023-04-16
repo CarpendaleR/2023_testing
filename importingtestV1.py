3fb306fa5ae85ebf5346b688e0924292930ce0d9
CarpendaleR <carpendaler@awatapu.school.nz>
2023-04-16 16:13:33 +1200

    initial commit, functions in other py files

---
+++ importingtestV1.py
@@ -0,0 +1,6 @@
+# CP test importing def
+
+def test_one_one(var1):
+    '''Things and stuff'''
+    var2 = var1 * var1
+    return var2
---
+++ main.py
@@ -0,0 +1,9 @@
+# Testing stuff all day
+
+
+#importing
+from importingtestV1 import test_one_one
+
+var3 = int(input("Number"))
+var4 = test_one_one(var3)
+print(f"Answer= {var4}")
