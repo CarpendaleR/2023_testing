5ffab296e848662de42db375cbdb8e15a681c298
CarpendaleR <carpendaler@awatapu.school.nz>
2023-04-16 16:32:01 +1200

    try/except for int input

--- main.py
+++ main.py
@@ -4,6 +4,17 @@
 #importing
 from importingtestV1 import test_one_one
 
+def func_is_it():
+    '''is it an int'''
+    try:
+        var5 = int(input(f"Input an integer"))
+    except:
+        print(f"Not an integer")
+        var5 = func_is_it()
+    return var5
+
 var3 = int(input("Number"))
 var4 = test_one_one(var3)
 print(f"Answer= {var4}")
+var5 = func_is_it()
+print(var5)
